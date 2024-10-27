65765c15d769c73f3f28a3a9a34b751e29a11c4b
Pratik4230 <pratikjadhav1438@gmail.com>
2024-10-27 11:05:42 +0530

    .

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'https://devflix-awrx.onrender.com',
     withCredentials: true
   });
