cbef18bfde63c05c7eb5062b8701fcee40802017
Pratik4230 <pratikjadhav1438@gmail.com>
2024-10-31 10:46:45 +0530

    implemented pagination

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,48 +1,79 @@
-
-import { axiosInstance } from '../utils/axiosInstance'
-import { useQuery } from '@tanstack/react-query';
-
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { axiosInstance } from '../utils/axiosInstance';
 import Shimmer from '../components/Shimmer';
-
 import { Link } from 'react-router-dom';
-
 import Videocarrd from '../components/Videocarrd';
-
+import { useEffect } from 'react';
+import { LoaderPinwheel } from 'lucide-react';
 
 const Feed = () => {
-
-
-  const {data:feed , isLoading: feedLoading, isError: feedError} = useQuery({
-    queryKey: ['feed'],
-    queryFn: async () => {
-      const response = await axiosInstance.get('/video/feed');
-      return response.data
+  const {
+    data: feed,
+    isLoading,
+    isError,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery({
+   queryKey:['feed'],
+    queryFn: async ({ pageParam = 1 }) => {
+      const response = await axiosInstance.get(`/video/feed?page=${pageParam}&limit=15`);
+      return response.data;
     },
-    staleTime: 1000 * 60 * 5,
+      getNextPageParam: (lastPage) => {
+        if (lastPage.videos.length === 0) return undefined; 
+        return lastPage.currentPage + 1; 
+      },
+      staleTime: 1000 * 60 * 5,
     
-  });
+});
 
-  if (feedLoading) return <Shimmer />
-      
+ 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (
+        window.innerHeight + document.documentElement.scrollTop + 5  >=
+        document.documentElement.scrollHeight
+      ) {
+        if (hasNextPage && !isFetchingNextPage) {
+          fetchNextPage();
+        }
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [hasNextPage, isFetchingNextPage, fetchNextPage]);
+
   
-  
-  if (feedError) {
-    return <div>Error</div>
+  if (isLoading) return <Shimmer />;
+
+ 
+  if (isError) {
+    return <div>Error loading feed.</div>;
   }
 
-  if (!feed || feed?.length === 0) {
+  console.log("feed", feed);
+  
+  if (!feed || feed.pages[0]?.videos.length === 0) {
     return <p>No videos found.</p>;
   }
 
   return (
-    
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 px-4 pt-6 bg-gray-100 dark:bg-black">
-    {feed?.videos?.map((video) => ( 
-     <Link to={`/video/${video._id}`} key={video._id}  > <Videocarrd  video={video} /> </Link>
-    ))}
-   
-  </div>
-  )
-}
+      {feed.pages.map((page) => (
+        page.videos.map((video) => (
+          <Link to={`/video/${video._id}`} key={video._id}>
+            <Videocarrd video={video} />
+          </Link>
+        ))
+      ))}
+      <p className='flex justify-center items-center' > {isFetchingNextPage &&   < LoaderPinwheel className='animate-spin flex justify-center items-center text-blue-500' size={33} /> } </p>
+      
+    </div>
+  );
+};
 
-export default Feed
+export default Feed;
